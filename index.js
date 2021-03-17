6e13fce1941983969b48384cb4a5d2559289edd4
Shadow <william.shadow@outlook.com>
2021-03-17 20:06:17 +0000

    need fs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const package = require("./package.json")
 const Discord = require("discord.js")
 const defaults = require("./defaults.js")
+const fs = require("fs")
 
 let saResources = {}
 
@@ -10,6 +11,11 @@ module.exports =
 
 module.exports.init = (client, options = defaults) => {
     if(!client.readyTimestamp) throw new Error("ShadowAdmin must be initialized inside the ready event of the client!")
+    
+    for(let x in defaults){
+        if(!options[x]) options[x] = defaults[x]
+    }
+
     saResources = {client, options, logger, exported: module.exports}
     logger(`ShadowAdmin v${package.version} initialized.`)
     if(options.messageHandler) require(`./messageHandler.js`)(saResources)
